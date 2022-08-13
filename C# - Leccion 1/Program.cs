4240c99df25261fdaf0efb961548bb3d2e21cb42
Tangoi7 <marianonn7@gmail.com>
2022-08-13 14:33:08 -0300

    Primera Leccion de Open Bootcamp - Tangoi7

---
+++ C# - Leccion 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ejercicio 1 - Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Console.WriteLine("¿Cual es tu nombre?");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Su nombre es: " + nombre);
+
+// Ejercicio 2 - Escribe un programa que tome la hora y la escriba por pantalla.
+
+Console.WriteLine("¿Cual es la hora?");
+
+string hora = DateTime.Now.ToString();
+
+Console.WriteLine(hora);
+
+// tangoi7
